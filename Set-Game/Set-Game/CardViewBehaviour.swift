4f20eadcf9fa17da01727fc3b29a57d0c3c83602
Madalina Sinca <madalinasinca@Madalinas-MacBook-Pro.local>
2019-08-21 18:07:59 +0300

    first commit  - adding behaviour to card views

---
+++ Set-Game/Set-Game/CardViewBehaviour.swift
@@ -0,0 +1,13 @@
+//
+//  CardViewBehaviour.swift
+//  Set-Game
+//
+//  Created by Madalina Sinca on 21/08/2019.
+//  Copyright © 2019 Madalina Sinca. All rights reserved.
+//
+
+import UIKit
+
+class CardViewBehaviour: UIDynamicBehavior {
+
+}
